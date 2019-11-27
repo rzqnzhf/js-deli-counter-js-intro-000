7f1d96f4ad98c173a6b18236127ac339c3d0fedd
Skin <rizqinnazhif@gmail.com>
2019-11-27 18:24:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  var lineNumber = katzDeliLine.length + 1;
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${lineNumber} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    var current = katzDeliLine.shift();
+    return `Currently serving ${current}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length > 1) {
+    var name = line.shift();
+    var output = 'The line is currently: ';
+    for (var i = 0; i <= line.length; i++) {
+      var addOutput = `${i}. ${name}, `;
+      var x = output.concat(addOutput);
+      return x;
+    }
+
+    /*
+    var i = 1;
+    var indexName = i - 1;
+    var baseOutput = `The line is currently: `;
+    while (i <= line.length) {
+      var modifiedLineOutput = `${i}. ${line[indexName]}, `;
+      i++;
+    */
+
+  } else {
+      return 'The line is currently empty.'
+    }
+}
